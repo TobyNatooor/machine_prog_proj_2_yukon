8eba4c31d4619bb4d72be9d65a6a12141f2db1c1
Marcus Lemser <s230985@dtu.dk>
2024-04-24 11:08:41 +0200

    More tests

--- src/tests/model/test_card_llist.c
+++ src/tests/model/test_card_llist.c
@@ -1,16 +1,20 @@
 #include "../../main/model/card_llist.h"
 #include <assert.h>
 
-void test1(void);
-void test2(void);
+void linkTwoCardsFindLast(void);
+void linkThreeCardsFindLast(void);
+void addOneCardToNullPointer(void);
+void addTwoCardsToNullPointer(void);
 
 int main(void) {
-    test1();
-    test2();
+    linkTwoCardsFindLast();
+    linkThreeCardsFindLast();
+    addOneCardToNullPointer();
+    addTwoCardsToNullPointer();
     return 0;
 }
 
-void test1(void){
+void linkTwoCardsFindLast(void){
     struct card_llist *card1 = (struct card_llist *)malloc(sizeof(struct card_llist));
     struct card_llist *card2 = (struct card_llist *)malloc(sizeof(struct card_llist));
     int expectedValue = 2;
@@ -22,7 +26,9 @@ void test1(void){
     card1->next = card2;
     card2->next = NULL;
 
+    //Testing get_last_card() function
     struct card_llist cardLast = *get_last_card(card1);
+
     int foundValue = cardLast.value;
     char foundSuit = cardLast.suit;
     assert(foundSuit == expectedSuit && foundValue == expectedValue);
@@ -30,22 +36,79 @@ void test1(void){
     free(card2);
 }
 
-void test2(void){
+
+void linkThreeCardsFindLast(void){
     struct card_llist *card1 = (struct card_llist *)malloc(sizeof(struct card_llist));
     struct card_llist *card2 = (struct card_llist *)malloc(sizeof(struct card_llist));
-    int expectedValue = 2;
+    struct card_llist *card3 = (struct card_llist *)malloc(sizeof(struct card_llist));
+    int expectedValue = 3;
     char expectedSuit = 'C';
     card1->value = 1;
     card1->suit = 'H';
-    card2->value = 3;
-    card2->suit = expectedSuit;
+    card2->value = 2;
+    card2->suit = 'S';
+    card3->value = expectedValue;
+    card3->suit = expectedSuit;
     card1->next = card2;
-    card2->next = NULL;
+    card2->next = card3;
+    card3->next = NULL;
 
+    // Testing get_last_card() funtion
     struct card_llist cardLast = *get_last_card(card1);
+
     int foundValue = cardLast.value;
     char foundSuit = cardLast.suit;
     assert(foundSuit == expectedSuit && foundValue == expectedValue);
     free(card1);
     free(card2);
+    free(card3);
 }
+
+void addOneCardToNullPointer(void){
+    struct card_llist **list = (struct card_llist *)malloc(sizeof(struct card_llist));
+    *list = NULL;
+    struct card_llist *card = (struct card_llist *)malloc(sizeof(struct card_llist));
+    int expectedValue = 3;
+    char expectedSuit = 'C';
+    card->value = expectedValue;
+    card->suit = expectedSuit;
+    card->next = NULL;
+
+    // Testing add_card() funtion
+    add_card(list, card);
+
+    struct card_llist cardLast = *get_last_card(*list);
+    int foundValue = cardLast.value;
+    char foundSuit = cardLast.suit;
+    assert(foundSuit == expectedSuit && foundValue == expectedValue);
+    free(card);
+    free(list);
+}
+
+void addTwoCardsToNullPointer(void){
+    struct card_llist **list = (struct card_llist *)malloc(sizeof(struct card_llist));
+    *list = NULL;
+    struct card_llist *card1 = (struct card_llist *)malloc(sizeof(struct card_llist));
+    struct card_llist *card2 = (struct card_llist *)malloc(sizeof(struct card_llist));
+    int expectedValue = 3;
+    char expectedSuit = 'C';
+    card1->value = 1;
+    card1->suit = 'H';
+    card1->next = NULL;
+    card2->value = expectedValue;
+    card2->suit = expectedSuit;
+    card2->next = NULL;
+
+    // Testing add_card() funtion
+    add_card(list, card1);
+    add_card(list, card2);
+
+    struct card_llist cardLast = *get_last_card(*list);
+    int foundValue = cardLast.value;
+    char foundSuit = cardLast.suit;
+    assert(foundSuit == expectedSuit && foundValue == expectedValue);
+    free(card1);
+    free(card2);
+    free(list);
+}
+
