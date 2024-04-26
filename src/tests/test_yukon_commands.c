989b70ba22f71a830b1aca32b91a7a7624eb3202
Tobias <s235086@student.dtu.dk>
2024-04-26 11:08:30 +0200

    added more tests

--- src/tests/test_yukon_commands.c
+++ src/tests/test_yukon_commands.c
@@ -5,6 +5,15 @@
 int main(void)
 {
     test_load_cards();
+    test_show_deck();
+    test_shuffle_si();
+    test_shuffle_sr();
+    test_save_deck();
+    test_quit_application();
+    test_init_play_phase();
+    test_quit_game();
+    test_move_cards_from_input();
+    test_handle_input();
     return 0;
 }
 
@@ -91,7 +100,7 @@ void test_show_deck()
     for (int i = 0; i < CARD_COUNT; i++)
     {
         assert(deck[i] != NULL);
-        assert(deck[i]->hidden == 0);
+        assert(deck[i]->hidden == 1);
     }
 
     response = show_deck(deck);
@@ -99,7 +108,7 @@ void test_show_deck()
     for (int i = 0; i < CARD_COUNT; i++)
     {
         assert(deck[i] != NULL);
-        assert(deck[i]->hidden == 1);
+        assert(deck[i]->hidden == 0);
     }
 }
 
@@ -107,14 +116,134 @@ void test_shuffle_si() {}
 
 void test_shuffle_sr() {}
 
-void test_save_deck() {}
+void test_save_deck()
+{
+    struct card_llist *deck[CARD_COUNT];
+    struct card_llist *columns[COLUMNS];
+    for (int i = 0; i < CARD_COUNT; i++)
+        deck[i] = NULL;
+    for (int i = 0; i < COLUMNS; i++)
+        columns[i] = NULL;
 
-void test_quit_application() {}
+    char *response = load_cards(columns, deck, NULL);
+    assert(strcmp(response, "OK") == 0);
 
-void test_init_play_phase() {}
+    response = save_deck(deck, "test_save_deck_deck_1.txt");
+    assert(strcmp(response, "OK") == 0);
 
-void test_quit_game() {}
+    const char cards[CARD_COUNT][CARD_SIZE] = {"AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "TC", "JC", "QC", "KC",
+                                               "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "TD", "JD", "QD", "KD",
+                                               "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "TH", "JH", "QH", "KH",
+                                               "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "TS", "JS", "QS", "KS"};
+    FILE *saved_file = fopen("test_save_deck_deck_1.txt", "r");
+    assert(saved_file != NULL);
+    for (int i = 0; i < CARD_COUNT; i++)
+    {
+        char character;
+        for (int j = 0; (character = fgetc(saved_file)) != '\n'; j++)
+            if (j == 0)
+                assert(character == cards[i][0]);
+            else if (j == 1)
+                assert(character == cards[i][1]);
+    }
+    fclose(saved_file);
+}
 
-void test_move_cards_from_input() {}
+void test_quit_application()
+{
+    struct card_llist *deck[CARD_COUNT];
+    struct card_llist *columns[COLUMNS];
+    struct card_llist *foundations[FOUNDATIONS];
+    for (int i = 0; i < CARD_COUNT; i++)
+        deck[i] = NULL;
+    for (int i = 0; i < COLUMNS; i++)
+        columns[i] = NULL;
+    for (int i = 0; i < FOUNDATIONS; i++)
+        foundations[i] = NULL;
+    int playing = 1;
+    char *response = load_cards(columns, deck, NULL);
+    assert(strcmp(response, "OK") == 0);
+    quit_application(foundations, columns, &playing);
+    assert(playing == 0);
+}
 
-void test_handle_input() {}
+void test_init_play_phase()
+{
+    struct card_llist *deck[CARD_COUNT];
+    struct card_llist *columns[COLUMNS];
+    for (int i = 0; i < CARD_COUNT; i++)
+        deck[i] = NULL;
+    for (int i = 0; i < COLUMNS; i++)
+        columns[i] = NULL;
+    int inPlayPhase = 0;
+    char *response = load_cards(columns, deck, NULL);
+    assert(strcmp(response, "OK") == 0);
+    response = init_play_phase(columns, deck, &inPlayPhase);
+    assert(strcmp(response, "OK") == 0);
+    const int columnSizes[COLUMNS] = {1, 6, 7, 8, 9, 10, 11};
+    for (int i = 0; i < COLUMNS; i++)
+        assert(get_cards_size(columns[i]) == columnSizes[i]);
+    response = init_play_phase(columns, deck, &inPlayPhase);
+    assert(strcmp(response, "Already in PLAY phase") == 0);
+}
+
+void test_quit_game()
+{
+    struct card_llist *deck[CARD_COUNT];
+    struct card_llist *columns[COLUMNS];
+    struct card_llist *foundations[FOUNDATIONS];
+    for (int i = 0; i < CARD_COUNT; i++)
+        deck[i] = NULL;
+    for (int i = 0; i < COLUMNS; i++)
+        columns[i] = NULL;
+    for (int i = 0; i < FOUNDATIONS; i++)
+        foundations[i] = NULL;
+    int inPlayPhase = 1;
+    char *response = load_cards(columns, deck, NULL);
+    assert(strcmp(response, "OK") == 0);
+    response = quit_game(foundations, columns, deck, &inPlayPhase);
+    assert(strcmp(response, "OK") == 0);
+    for (int i = 0; i < CARD_COUNT; i++)
+        assert(deck[i]->hidden == 1);
+    for (int i = 0; i < FOUNDATIONS; i++)
+        assert(foundations[i] == NULL);
+    assert(inPlayPhase == 0);
+    response = quit_game(foundations, columns, deck, &inPlayPhase);
+    assert(strcmp(response, "Can't quit game if not in PLAY phase") == 0);
+}
+
+void test_move_cards_from_input()
+{
+    struct card_llist *deck[CARD_COUNT];
+    struct card_llist *columns[COLUMNS];
+    struct card_llist *foundations[FOUNDATIONS];
+    for (int i = 0; i < CARD_COUNT; i++)
+        deck[i] = NULL;
+    for (int i = 0; i < COLUMNS; i++)
+        columns[i] = NULL;
+    for (int i = 0; i < FOUNDATIONS; i++)
+        foundations[i] = NULL;
+    int inPlayPhase = 0;
+
+    char *response = load_cards(columns, deck, NULL);
+    assert(strcmp(response, "OK") == 0);
+    response = init_play_phase(columns, deck, &inPlayPhase);
+    assert(strcmp(response, "OK") == 0);
+    response = move_cards_from_input(columns, foundations, "C8:0->F1");
+    assert(strcmp(response, "Invalid column number") == 0);
+    response = move_cards_from_input(columns, foundations, "F5:0->C5");
+    assert(strcmp(response, "Invalid foundation number") == 0);
+    response = move_cards_from_input(columns, foundations, "X1:0->F1");
+    assert(strcmp(response, "Invalid command") == 0);
+    response = move_cards_from_input(columns, foundations, "C1->F1");
+    assert(strcmp(response, "OK") == 0);
+    assert(columns[0] == NULL);
+    assert(foundations[0] != NULL);
+    assert(foundations[0]->value == 1);
+    assert(foundations[0]->suit == 'C');
+}
+
+void test_handle_input() 
+{
+    
+}
