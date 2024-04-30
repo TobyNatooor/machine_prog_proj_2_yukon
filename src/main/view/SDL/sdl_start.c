65ffb47bda7f528baa8242ebf63f1a0f78c10442
Tobias <s235086@student.dtu.dk>
2024-04-30 21:03:39 +0200

    add fonts and take user input

--- src/main/view/SDL/sdl_start.c
+++ src/main/view/SDL/sdl_start.c
@@ -1,26 +1,68 @@
-#include "sdl_gui.h"
+#include <SDL.h>
+#include <SDL_ttf.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init();
 
-    game_is_running = initialize_window();
-    setup_card_frames();
-    render();
+    SDL_Window *window = SDL_CreateWindow("SDL_ttf Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    struct card_llist *deck[CARD_COUNT];
-    struct card_llist *columns[COLUMNS];
-    struct card_llist *foundations[FOUNDATIONS];
-    for (int i = 0; i < CARD_COUNT; i++)
-        deck[i] = NULL;
-    for (int i = 0; i < COLUMNS; i++)
-        columns[i] = NULL;
-    for (int i = 0; i < FOUNDATIONS; i++)
-        foundations[i] = NULL;
+    TTF_Font *font = TTF_OpenFont("../fonts/PlayFairDisplay-Regular.ttf", 24);
+    SDL_Color color = {255, 255, 255}; // White
 
-    while(game_is_running) {
-        process_input(columns,deck,myRenderer,NULL);
+    char text[100] = "Type something!";
+    SDL_Surface *textSurface = TTF_RenderText_Solid(font, text, color);
+    SDL_Texture *textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+
+    int running = 0;
+
+    SDL_Event e;
+    while (!running)
+    {
+        while (SDL_PollEvent(&e) != 0)
+        {
+            if (e.type == SDL_QUIT)
+            {
+                running = 1;
+            }
+            else if (e.type == SDL_KEYDOWN)
+            {
+                if (e.key.keysym.sym == SDLK_BACKSPACE && strlen(text) > 0)
+                {
+                    text[strlen(text) - 1] = '\0';
+                }
+                else if (e.key.keysym.sym >= SDLK_SPACE && e.key.keysym.sym <= SDLK_z)
+                {
+                    char newChar[2] = {e.key.keysym.sym, '\0'};
+                    strcat(text, newChar);
+                }
+
+                // Re-render text
+                SDL_FreeSurface(textSurface);
+                SDL_DestroyTexture(textTexture);
+                textSurface = TTF_RenderText_Solid(font, text, color);
+                textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+            }
+        }
+
+        SDL_RenderClear(renderer);
+        if (textTexture)
+        {
+            SDL_Rect renderQuad = {0, 0, textSurface->w, textSurface->h};
+            SDL_RenderCopy(renderer, textTexture, NULL, &renderQuad);
+        }
+        SDL_RenderPresent(renderer);
     }
 
-    destroy_window();
+    SDL_FreeSurface(textSurface);
+    SDL_DestroyTexture(textTexture);
+    TTF_CloseFont(font);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    TTF_Quit();
+    SDL_Quit();
 
     return 0;
 }
